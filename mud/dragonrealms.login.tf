ec1d1ffe18ab398f1662fb9902ec22060d9d5acd
Greg Millam <captdeaf@gmail.com>
2015-03-23 14:45:57 -0700

    Complete the dragonrealms connection, rather than wait for player.

--- mud/dragonrealms.login.tf
+++ mud/dragonrealms.login.tf
@@ -2,7 +2,6 @@
 /def -p9999 -mglob -h'CONNECT dr' \
 	hook_DR=/quote -S -wdr \
         !"~/.tf/mud/dragonrealms.login.pl ACCOUNTNAME PASSWORD CHARACTER 2>/dev/null" %; \
-        /send -wdr "/FE:WIZARD /VERSION:2.02 /P:WIN32" %;\
-        /send -wdr %;\
-        /send -wdr %;\
+        /repeat -1 1 send -wdr "/FE:WIZARD /VERSION:2.02 /P:WIN32" %;\
+        /repeat -2 2 send -wdr %;\
 	/set emulation=ansi_attr
