5f39db8382922269fa5c19e26efc832404d31663
Greg Millam <captdeaf@gmail.com>
2015-03-24 13:02:49 -0700

    Change from all to dr-load

---
+++ mud/dr-load.tf
@@ -0,0 +1,7 @@
+; /load_dr <path to dr>
+/def load_dr=\
+  /require %{*}/dr.tf %;\
+  /require %{*}/dr-autopick.tf %;\
+  /require %{*}/dr-vaultswap.tf %;\
+  /require %{*}/dr-crafting.tf %;\
+  /require %{*}/dr-colors.tf
