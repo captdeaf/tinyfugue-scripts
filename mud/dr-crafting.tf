df19914d262f962620c5046628ff2f47a4e7680a
Greg Millam <captdeaf@gmail.com>
2015-03-23 19:28:59 -0700

    Add vaultswap

--- mud/dr-crafting.tf
+++ mud/dr-crafting.tf
@@ -7,6 +7,26 @@
 ; craft item, maybe eventually macro it.
 ; /def -p10 -h"SEND craft *" -wdr dr_craft=???
 
+; Swapping backpacks in the vault.
+; I have:
+;   tan: Tailoring
+;   red: forging
+;   black: engineering
+;   green: adventuring (default)
+;
+; vaultswap <color>, while standing in carousel, will go through ironwood
+; arch, pull lever, etc etc, place all backpacks in vaults, then get
+; the desired backpack. It also auto-sets the "store default"
+/def -p10 -h"SEND vaultswap *" -mglob -wdr dr_vault_swap_backpack=\
+  /let color=%{-1} %;\
+  /dr_stow %{dr_lhand} %;\
+  /dr_stow %{dr_rhand} %;\
+  /drc go ironwood arch=pull lever=go door=open vault %;\
+  /drc remove backpack=put my backpack in vault%;\
+  /drc get %{color} backpack from vault %;\
+  /drc wear my backpack=store default in my backpack %;\
+  /drc close vault=go door=go arch
+
 ; Tailoring.
 /def -p10 -h"SEND tcut * *" -mglob -wdr dr_tailor_cut=\
   /let mat=%{-2} %;\
