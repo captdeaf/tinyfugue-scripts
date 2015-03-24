499a6341783dc75bd72bb21907d3088efeb873d1
Greg Millam <captdeaf@gmail.com>
2015-03-24 01:21:05 -0700

    Vault swapping for crafting backpacks.

---
+++ mud/dr-vaultswap.tf
@@ -0,0 +1,10 @@
+/def -p10 -h"SEND vaultswap *" -mglob -wdr dr_vault_swap_backpack=\
+  /let color=%{-1} %;\
+  /dr_stow %{dr_lhand} %;\
+  /dr_stow %{dr_rhand} %;\
+  /drc go ironwood arch=pull lever=go door=open vault %;\
+  /drc remove backpack=put my backpack in vault%;\
+  /drc get %{color} backpack from vault %;\
+  /drc wear my backpack=store default in my backpack %;\
+  /drc close vault=go door=go arch
+
