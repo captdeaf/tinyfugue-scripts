fb14ef5513677300ccb5a25c5295e50b3b5928b6
Greg Millam <captdeaf@gmail.com>
2015-03-25 02:56:23 -0700

    A different approach to dr-loading.

--- mud/dr-load.tf
+++ mud/dr-load.tf
@@ -1,7 +1,9 @@
 ; /load_dr <path to dr>
 /def load_dr=\
-  /require %{*}/dr.tf %;\
-  /require %{*}/dr-autopick.tf %;\
-  /require %{*}/dr-vaultswap.tf %;\
-  /require %{*}/dr-crafting.tf %;\
-  /require %{*}/dr-colors.tf
+  /set dr_path=%{*} %;\
+  /require %{dr_path}/dr.tf %;\
+  /require %{dr_path}/dragonrealms.login.tf %;\
+  /require %{dr_path}/dr-autopick.tf %;\
+  /require %{dr_path}/dr-vaultswap.tf %;\
+  /require %{dr_path}/dr-crafting.tf %;\
+  /require %{dr_path}/dr-colors.tf
