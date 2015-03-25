8230ca60d0bc15d33509496e5b0cda0ffdef7c8e
Greg Millam <captdeaf@gmail.com>
2015-03-25 04:17:01 -0700

    Start of work to make dtrain

--- mud/dr-scripts.tf
+++ mud/dr-scripts.tf
@@ -1,21 +1,15 @@
-; Just stuff in here that should eventually be made into a script.
-;
-; Generic loaded-aimed-fired weapon in combat:
+; slr: Stow left and right.
+/def -p10 -h"SEND dtrain" -wdr dr_dtrain=\
+  /echo ## Combat options: %;\
+  /echo ## %;\
+  /echo ## dtrain bow: stance to shield, wield bow. cycle: load, aim, dagger, hammer, hide, hunt. spells: ecc 8 %;\
+  /echo ## dtrain sling: Like bow, but with a sling. %;\
+  /echo ## dtrain edge: Wield telek in right hand, stance to parry. cycle: dagger, hammer, hunt, hide, analyze. spells: devi 8%;\
+  /echo ## dtrain club: Wield cudgel in right hand, stance to parry. cycle: dagger, hammer, hunt, hide, analyze. spells: devi 8%;\
+  /echo ## %;\
+  /echo ## dtrain outdoor: hold runestone. %;\
+  /echo ##                 cycle: collect rock+kick pile, appraise runestone, focus runestone, appraise coat,%;\
+  /echo ##                        perceive, appraise buckler, hunt, perform.%;\
+  /echo ##                spells: stw 8, compost 8, ey 8%;\
+  /echo ## %;\
 
-/set dr_cycle=load=~=aim=~=hunt=~=hide=~=hunt
-
-; Ditto, but throwing with left hand.
-/set dr_cycle=load=~=aim=~=get throwing dagger=~=lob left=~=hide=~=hunt
-
-; Throwing both hammer and dagger in combat, with hide and appraise trained.
-/set dr_cycle=get hammer=get throwing dagger=hide=~=lob right=~=lob left=~=appraise ENEMY
-
-; Melee
-/set dr_cycle=hide=~=analyze=~=prep ec 4=~=hunt
-
-
-; In-town, train almost everything not combat-related.
-; Need to be holding a runestone and wearing coat.
-; Need to be in a place to collect rocks.
-
-/set dr_cycle=hunt=~=collect rock=~=prep stw 4=~=kick pile=~=scout cover my tracks=~=appraise runestone=~=collect rock=~=kick pile=hunt=~=focus runestone=~=prep compost 4=~=focus runestone=~=appraise coat=~=perceive=~=collect rock=~=kick pile=~=prep ey 5=~=focus runestone=~=appraise buckler
