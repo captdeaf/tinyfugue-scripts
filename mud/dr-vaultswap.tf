508372215314ddcf779f96cc482edaa378f70ba9
Greg Millam <captdeaf@gmail.com>
2015-03-28 17:15:27 -0700

    Attempt all arches in case of busy room.

--- mud/dr-vaultswap.tf
+++ mud/dr-vaultswap.tf
@@ -2,7 +2,7 @@
   /let color=%{-1} %;\
   /dr_stow %{dr_lhand} %;\
   /dr_stow %{dr_rhand} %;\
-  /drc go ironwood arch=pull lever=go door=open vault %;\
+  /drc go ironwood arch=go blackwood arch=go rosewood arch=pull lever=go door=open vault %;\
   /drc remove backpack=put my backpack in vault%;\
   /drc get %{color} backpack from vault %;\
   /drc wear my backpack=store default in my backpack %;\
