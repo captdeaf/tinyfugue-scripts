43c64222aa85a47bcc080fe38ec1653d1343d44f
Greg Millam <captdeaf@gmail.com>
2015-03-28 17:16:44 -0700

    Some dtrain attach changes.

--- mud/dr-scripts.tf
+++ mud/dr-scripts.tf
@@ -6,12 +6,11 @@
   /echo ## %;\
   /echo ## dtrain bow: stance to shield, wield bow. cycle: load, aim, dagger, hammer, hide, hunt. spells: ec 8 %;\
   /echo ## dtrain sling: Like bow, but with a sling. %;\
-  /echo ## dtrain edge: Wield telek in right hand, stance to parry. cycle: dagger, hammer, hunt, hide, analyze. spells: devi 8%;\
-  /echo ## dtrain blunt: Wield cudgel in right hand, stance to parry. cycle: dagger, hammer, hunt, hide, analyze. spells: devi 8%;\
+  /echo ## dtrain melee (weapon): Wield weapon in right hand, stance to parry. cycle: dagger, hammer, hunt, hide, analyze. spells: devi 8%;\
   /echo ## %;\
   /echo ## dtrain outdoor: hold runestone. %;\
-  /echo ##                 cycle: collect rock+kick pile, appraise runestone, focus runestone, appraise coat,%;\
-  /echo ##                        perceive, appraise buckler, hunt, perform.%;\
+  /echo ##                 cycle: collect rock+kick pile, appraise runestone, focus runestone, %;\
+  /echo ##                        perceive, appraise buckler, hunt, appraise gweth.%;\
   /echo ##                spells: stw 8, compost 8, ey 8%;\
   /echo ## %;\
   /echo ## dtrain done: The clean way to end these, rather than 'oops' %;\
@@ -39,7 +38,7 @@
   /dr_spell_add prepare stw 8 %;\
   /dr_spell_add prepare ey 8 %;\
   /set dr_spell_cycle=target ec 8 %;\
-  /set dr_cycle=load=~=aim=get throwing dagger=lob left=get throwing hammer=throw left=~=hide=~=hunt %;\
+  /set dr_cycle=load=~=aim=get throwing dagger=lob left=~=get throwing hammer=throw left=~=hide=~=hunt %;\
   /set dr_dtrain_on_complete=stow throwing dagger=stow throwing hammer
 
 /def -p10 -h"SEND dtrain sling" -wdr dr_dtrain_sling=\
@@ -49,25 +48,17 @@
   /dr_spell_add prepare stw 8 %;\
   /dr_spell_add prepare ey 8 %;\
   /set dr_spell_cycle=target ec 8 %;\
-  /set dr_cycle=load=~=aim=get throwing dagger=lob left=get throwing hammer=throw left=~=hide=~=hunt %;\
+  /set dr_cycle=load=~=aim=get throwing dagger=lob left=~=get throwing hammer=throw left=~=hide=~=hunt %;\
   /set dr_dtrain_on_complete=stow throwing dagger=stow throwing hammer
 
-/def -p10 -h"SEND dtrain edge" -wdr dr_dtrain_edge=\
+/def -p10 -h"SEND dtrain melee *" -wdr dr_dtrain_melee=\
+  /let weapon=%{-2} %;\
   /drc /dr_stow_hands %;\
-  /drc wield telek %;\
+  /drc wield %{weapon} %;\
   /drc stance set 100 80 0 100 %;\
   /dr_spell_add prepare ey 8 %;\
   /set dr_spell_cycle=target devi 8 %;\
-  /set dr_cycle=get throwing dagger=lob left=get throwing hammer=throw left=~=hunt=~=hide=~=analyze=~=~ %;\
-  /set dr_dtrain_on_complete=stow throwing dagger=stow throwing hammer
-
-/def -p10 -h"SEND dtrain blunt" -wdr dr_dtrain_blunt=\
-  /drc /dr_stow_hands %;\
-  /drc wield cudgel %;\
-  /drc stance set 100 80 0 100 %;\
-  /dr_spell_add prepare ey 8 %;\
-  /set dr_spell_cycle=target devi 8 %;\
-  /set dr_cycle=get throwing dagger=lob left=get throwing hammer=throw left=~=hunt=~=hide=~=analyze=~=~ %;\
+  /set dr_cycle=get throwing dagger=lob left=~=get throwing hammer=throw left=~=hunt=~=hide=~=analyze=~=~ %;\
   /set dr_dtrain_on_complete=stow throwing dagger=stow throwing hammer
 
 /def -p10 -h"SEND dtrain outdoor" -wdr dr_dtrain_outdoor=\
@@ -76,3 +67,12 @@
   /set dr_spell_cycle=prep stw 8=prep compost 8=prep ey 8 %;\
   /set dr_cycle=collect rock=~=kick pile=~=appraise runestone=~=focus runestone=~=appraise buckler=~=perceive=~=hunt=~=appraise gweth %;\
   /set dr_dtrain_on_complete=/dr_stow_hands
+
+; Clear queue, send complete, save cycle to "fight"
+; There is nothing else to face!
+/def -t"There is nothing else to face!" -mglob -wdr drt_stopcycle_fighting=\
+  /set dr_fight_cycle=%{dr_cycle} %;\
+  /set dr_fight_spell_cycle=%{dr_spell_cycle} %;\
+  /set dr_cycle= %;\
+  /set dr_spell_cycle= %;\
+  /drc %{dr_dtrain_on_complete}
